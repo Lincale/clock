179b1bff9ea4251135fc9bf8bf084ccfb8328095
Lincale <Linca.github@gmail.com>
2021-11-17 22:23:12 +0900

    不要なmodule, libraryを削除

--- src/hooks/useMagic.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { MagicContext } from "src/lib/magicContext";
-
-export const useMagic = () => useContext(MagicContext)
--- src/lib/magicContext.ts
+++
@@ -1,6 +0,0 @@
-import { Magic } from "magic-sdk";
-import { createContext } from "react";
-
-export const MagicContext = createContext<{
-  magicClient?: Magic;
-}>({})
