dc1fc426da5e3107e3309cb3f9b22a9fd94bb699
Lincale <Linca.github@gmail.com>
2021-11-27 17:10:19 +0900

    dynamic routingを使うための修正 他文言追加

--- src/components/clock/RealTimeClock.tsx
+++ src/components/clock/RealTimeClock.tsx
@@ -1,10 +1,11 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
+import { FlexibleBox } from '../common/FlexibleBox';
 
 export type RealTimeClockProps = {};
 
-export const RealTimeClock: React.FC<RealTimeClockProps> = ({ ...props }) => {
+const RealTimeClock: React.FC<RealTimeClockProps> = () => {
   const [clock, setClock] = useState(
     dayjs().tz().format('YYYY-MM-DDTHH:mm:ss')
   );
@@ -15,7 +16,14 @@ export const RealTimeClock: React.FC<RealTimeClockProps> = ({ ...props }) => {
     }, 1000);
 
     return () => clearInterval(timer);
-  });
+  }, [clock]);
 
-  return <Typography variant={'h4'}>{clock}</Typography>;
+  return (
+    <FlexibleBox>
+      <Typography variant={'h5'}>現在の時刻は</Typography>
+      <Typography variant={'h4'}>{clock}</Typography>
+    </FlexibleBox>
+  );
 };
+
+export default RealTimeClock;
