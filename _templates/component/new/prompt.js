38102044c45ae8cc784a037d08f536fc2ff43d1f
Lincale <Linca.github@gmail.com>
2021-11-27 23:48:11 +0900

    ファイル修正

--- _templates/component/new/prompt.js
+++ _templates/component/new/prompt.js
@@ -17,12 +17,4 @@ module.exports = [
     message: 'src/components以下のパスを指定してください。ex) common',
     validate: inputValidator,
   },
-  {
-    type: 'toggle',
-    name: 'withStory',
-    message: '.stories.tsxも一緒に作成しますか？',
-    disabled: 'いいえ',
-    enabled: 'はい',
-    initial: true,
-  },
 ];
