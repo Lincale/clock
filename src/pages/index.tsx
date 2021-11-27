ad6b2ee2b5c09a5ee3bfb7652fae6cc3acd7a593
Lincale <Linca.github@gmail.com>
2021-11-27 17:06:49 +0900

    サーバークライアント間で表示日付の差異が出るため、SSRを行わないよう修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
-import { Stack, Divider } from '@mui/material';
+import dynamic from 'next/dynamic';
+import { Stack, Divider, CircularProgress, Box } from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import { RealTimeClock } from 'src/components/clock/RealTimeClock';
+// import { RealTimeClock } from 'src/components/clock/RealTimeClock';
 import { Header } from 'src/components/common/Header';
 import { RandomUser } from 'src/components/user/RandomUser';
 
+const RealTimeClock = dynamic(
+  () => import('../components/clock/RealTimeClock'),
+  {
+    loading: () => <CircularProgress />,
+    ssr: false,
+  }
+);
+
 const Home: NextPage = () => {
   return (
-    <>
+    <Box>
       <Head>
         <title>Clock</title>
         <meta name='description' content='Clock' />
@@ -26,7 +35,7 @@ const Home: NextPage = () => {
         <RealTimeClock />
         <RandomUser />
       </Stack>
-    </>
+    </Box>
   );
 };
 
