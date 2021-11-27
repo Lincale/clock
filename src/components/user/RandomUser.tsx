4a51252e44a44c2c81c8d7610901afc82876a08e
Lincale <Linca.github@gmail.com>
2021-11-27 17:09:23 +0900

    カスタムフックに合わせた変更, 使用コンポーネントの変更 etc

--- src/components/user/RandomUser.tsx
+++ src/components/user/RandomUser.tsx
@@ -1,22 +1,18 @@
-import { Button, CircularProgress, Stack, Divider } from '@mui/material';
+import { Button, CircularProgress, Stack, Divider, Box } from '@mui/material';
 import React from 'react';
 import { useUserSwr } from 'src/hooks/useUserSwr';
 import { isUserArray, User } from 'src/types/User';
+import { FlexibleBox } from '../common/FlexibleBox';
 
 export type RandomUserProps = {};
 
 export const RandomUser: React.FC<RandomUserProps> = ({ ...props }) => {
-  const { data, mutate, isLoading, isError } = useUserSwr();
+  const { data, onClick, isLoading, isError } = useUserSwr();
 
   const RandomUserButton: React.FC = () => {
     return (
-      <Button
-        variant='contained'
-        onClick={() => {
-          mutate?.();
-        }}
-      >
-        天気を見る
+      <Button variant='contained' onClick={onClick}>
+        ランダムユーザー
       </Button>
     );
   };
@@ -30,9 +26,9 @@ export const RandomUser: React.FC<RandomUserProps> = ({ ...props }) => {
     );
 
   return (
-    <>
+    <FlexibleBox>
       <RandomUserButton />
-      <>
+      <Box>
         {data.results.map((el: User) => (
           <Stack
             key={`${el.name.first}-${el.name.last}`}
@@ -48,7 +44,7 @@ export const RandomUser: React.FC<RandomUserProps> = ({ ...props }) => {
             {`nat: ${el.nat}`}
           </Stack>
         ))}
-      </>
-    </>
+      </Box>
+    </FlexibleBox>
   );
 };
